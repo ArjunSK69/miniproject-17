dbdae606ff3d07fa139ecb7c90c35a2541dfc3d4
ArjunSK69 <15arjunsk19020618@gmail.com>
2024-05-09 22:50:39 +0530

    update personality description

--- careercrafter/models.py
+++ careercrafter/models.py
@@ -17,3 +17,23 @@ class users(db.Model, UserMixin):
 
     def __repr__(self):
         return f"users('{self.user_name}', '{self.email}', '{self.personality_type}')"
+
+class questions(db.Model):
+    q_id = db.Column(db.Integer, primary_key=True)
+    q_text = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"questions('{self.q_text}')"
+
+class careers(db.Model):
+    title = db.Column(db.String(50), primary_key=True)
+    courses = db.Column(db.String(200), unique=True, nullable=False)
+    institutions = db.Column(db.String(200), nullable=False)
+    entrance_exams = db.Column(db.String(80), nullable=True)
+
+    def __repr__(self):
+        return f"users('{self.title}', '{self.courses}', '{self.institutions}', '{self.entrance_exams}')"
+
+class d(db.Model):
+    personality_type = db.Column(db.String(5), primary_key=True)
+    description = db.Column(db.String(400), nullable=False)
